0e28de80f401c8620000c26fdfc4e479cd25c5ba
FeNiksleks <feniksleks@yandex.ru>
2023-09-03 21:13:42 +0300

    Task_01

---
+++ Task_01/src/com/company/User.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class User {
+    String login;
+    String password;
+    Basket userBasket;
+
+    public User() {
+        this.login = "Unknown login";
+        this.password = "qwerty";
+    }
+
+    public User setLogin(String login) {
+        this.login = login;
+        return this;
+    }
+
+    public User setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public User setBasket(Basket basket) {
+        this.userBasket = basket;
+        return this;
+    }
+
+    public void getUserBusket() {
+        System.out.println(String.format("username: %s,\n user's shopping list: %s",
+                this.login, userBasket.getShoppingList()));
+    }
+}
+
+
