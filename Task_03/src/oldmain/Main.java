d5b67dfef848101b0716840450eaf7b1990a9e12
FeNiksleks <feniksleks@yandex.ru>
2023-09-04 22:52:28 +0300

    Task_03

---
+++ Task_03/src/oldmain/Main.java
@@ -0,0 +1,24 @@
+package oldmain;
+
+import gamefiles.Answer;
+import gamefiles.GameStatus;
+import gamefiles.NumberGame;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        NumberGame numberGame = new NumberGame();
+        Scanner scanner = new Scanner(System.in);
+        numberGame.start(scanner.nextInt(), scanner.nextInt());
+        scanner.nextLine();
+        while (!numberGame.getGameStatus().equals(GameStatus.FINISH)){
+            String value = scanner.nextLine();
+            Answer answer = numberGame.inputValue(value);
+          if (answer!=null){
+              System.out.println("answer = " + answer);
+          }
+            System.out.println(numberGame.getMoveHistory());
+        }
+    }
+}
